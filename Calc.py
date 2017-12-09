64172013282017bacd4c6f32754a7ec67c9002ab
amar <amar@gmail.com>
2017-12-09 19:58:53 +0530

    fuck

--- Calc.py
+++
@@ -1,4 +0,0 @@
-def add(a,b)
-   pass
-def sub(a,b)
-   return a-b
--- seqdow.sh
+++
@@ -1,8 +0,0 @@
-
-k=0
- for i in  `cat $1` ; 
- do youtube-dl $i ; echo ; echo ; 
-	 k=`echo $k + 1 |bc -l` ; 
-	 echo $k ; 
-	 echo ;echo ;
- done
